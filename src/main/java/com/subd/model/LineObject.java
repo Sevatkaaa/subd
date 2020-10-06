677f71f35b7d804bfea8ed52b5298b775e05dc9d
alina-myrchenko <alina.myrchenko@binary-studio.com>
2020-10-06 13:21:57 +0300

    add method equals

--- src/main/java/com/subd/model/LineObject.java
+++ src/main/java/com/subd/model/LineObject.java
@@ -3,6 +3,7 @@ package com.subd.model;
 import lombok.Data;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Table(name = "lineObject")
@@ -18,4 +19,19 @@ public class LineObject {
     @ManyToOne
     @JoinColumn(name = "lineObjects", nullable = false)
     private Line line;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        LineObject that = (LineObject) o;
+        return Objects.equals(name, that.name) &&
+                type == that.type &&
+                Objects.equals(value, that.value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, type, value);
+    }
 }
